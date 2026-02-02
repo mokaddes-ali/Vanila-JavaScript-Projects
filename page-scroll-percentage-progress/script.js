2530f7154f38d7f58a1b69c4679b98b020623b92
Mokaddes Ali <mokaddes.ru2000@gmail.com>
2026-02-02 15:12:57 +0600

    show progress scroll top and buttom commit

--- page-scroll-percentage-progress/script.js
+++ page-scroll-percentage-progress/script.js
@@ -1,6 +1,7 @@
 let scrollPercentage = () => {
     let progress = document.getElementById("progress");
     let value = document.getElementById("progress-value");
+    let progressTop = document.getElementById("progress-top");
 
     let scrollTop = document.documentElement.scrollTop;
     let scrollHeight =
@@ -8,6 +9,8 @@ let scrollPercentage = () => {
         document.documentElement.clientHeight;
 
     let percent = Math.round((scrollTop / scrollHeight) * 100);
+    
+    progressTop.style.width = percent + "%";
 
     progress.style.background = `
         conic-gradient(
